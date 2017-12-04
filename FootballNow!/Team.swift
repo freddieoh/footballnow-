90efc194b6473fd6bc3d3fd92a538df1b4dc3b77
Fredrick Ohen <fredrickohen@gmail.com>
2017-12-04 14:34:08 -0600

    Added error handling to Team model

--- FootballNow!/Team.swift
+++ FootballNow!/Team.swift
@@ -13,6 +13,42 @@ struct Team {
   let name: String
   let code: String
   let shortName: String
-  let crestURL: String
+  let crestUrl: String
 
+}
+
+enum SerializationError: Error {
+  case missing(String)
+  case invalid(String, Any)
+  
+}
+
+
+extension Team {
+  
+  init(json: [String:Any]) throws {
+    guard let name = json["name"] as? String else {
+      throw SerializationError.missing("name")
+    }
+    
+    guard let code = json["code"] as? String else {
+      throw SerializationError.missing("code")
+    }
+    
+    guard let shortName = json["shortName"] as? String else {
+      throw SerializationError.missing("shortName")
+    }
+    
+    guard let crestUrl = json["crestUrl"] as? String else {
+      throw SerializationError.missing("crestUrl")
+    }
+    
+    self.name = name
+    self.code = code
+    self.shortName = shortName
+    self.crestUrl = crestUrl
+    
+  }
+  
+  
 }
