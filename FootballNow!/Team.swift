d6581fac705c2591f98b24bba696411849686873
Fredrick Ohen <fredrickohen@gmail.com>
2017-12-04 14:19:39 -0600

    first commit

---
+++ FootballNow!/Team.swift
@@ -0,0 +1,18 @@
+//
+//  Team.swift
+//  FootballNow!
+//
+//  Created by Fredrick Ohen on 12/4/17.
+//  Copyright © 2017 geeoku. All rights reserved.
+//
+
+import Foundation
+
+struct Team {
+  
+  let name: String
+  let code: String
+  let shortName: String
+  let crestURL: String
+
+}
