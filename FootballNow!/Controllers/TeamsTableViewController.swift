4fda878c60bb1136b9c3814f223d9fd86d2fabc0
Fredrick Ohen <fredrickohen@gmail.com>
2017-12-06 15:13:34 -0600

    Accessed all key/value pairs from teams dictionary

--- FootballNow!/Controllers/TeamsTableViewController.swift
+++ FootballNow!/Controllers/TeamsTableViewController.swift
@@ -13,6 +13,7 @@ class TeamsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       registerNibFiles()
+      retrieveTeamsFromApi()
 
     }
 
@@ -20,6 +21,41 @@ class TeamsTableViewController: UITableViewController {
   func registerNibFiles() {
     tableView.register(UINib(nibName: "TeamsTableViewCell", bundle: nil), forCellReuseIdentifier: "TeamsCell")
   }
+  
+  
+  func retrieveTeamsFromApi() {
+    let teamsURL = "http://api.football-data.org/v1/competitions/445/teams"
+    guard let url = URL(string: teamsURL) else { return }
+    let session = URLSession.shared
+    let task = session.dataTask(with: url) { (data, response, error) in
+      if let error = error {
+        DispatchQueue.main.async {
+          //make UI error Change
+          print(error.localizedDescription)
+        }
+        return
+      }
+      
+      guard let data = data else { return }
+      
+      let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+      guard let teams = json["teams"] as? [[String:Any]] else { return }
+      
+      for team in teams {
+        print(team["name"] as Any)
+      }
+    
+      
+      guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        DispatchQueue.main.async {
+          // Make UI error Change
+        }
+        return
+      }
+    }
+      task.resume()
+  }
+
   
   // MARK: - Table view data source
 
@@ -39,9 +75,9 @@ class TeamsTableViewController: UITableViewController {
       return cell
     }
   
-  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 220.5
-  }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+      return 220.5
+    }
 
 
 }
