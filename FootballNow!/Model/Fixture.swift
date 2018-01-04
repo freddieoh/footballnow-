e059abc80f945fe324988a5c4a94f34f099b95ba
Fredrick Ohen <fredrickohen@gmail.com>
2018-01-04 14:24:32 -0600

    Added League Table and Fixture to model

---
+++ FootballNow!/Model/Fixture.swift
@@ -0,0 +1,18 @@
+//
+//  Fixture.swift
+//  FootballNow!
+//
+//  Created by Fredrick Ohen on 1/4/18.
+//  Copyright © 2018 geeoku. All rights reserved.
+//
+
+import Foundation
+
+struct Fixture {
+  
+  let date: String
+  let homeTeamName: String
+  let awayTeamName: String
+  let result: String
+  
+}
---
+++ FootballNow!/Model/LeagueTable.swift
@@ -0,0 +1,23 @@
+//
+//  LeagueTable.swift
+//  FootballNow!
+//
+//  Created by Fredrick Ohen on 1/4/18.
+//  Copyright © 2018 geeoku. All rights reserved.
+//
+
+import Foundation
+
+struct LeagueTable {
+  
+  let position: Int
+  let teamName: String
+  let crestURL: String
+  let playedGames: Int
+  let points: Int
+  let goals: Int
+  let wins: Int
+  let draws: Int
+  let losses: Int
+
+}
