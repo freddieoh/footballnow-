34aa26ac4af8659ce23eefa8e8c9e0c3c0e60d8a
Fredrick Ohen <fredrickohen@gmail.com>
2018-01-04 15:07:09 -0600

    Added League TableViewController and Xib

---
+++ FootballNow!/Controller/LeagueTableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LeagueTableViewController.swift
+//  FootballNow!
+//
+//  Created by Fredrick Ohen on 1/4/18.
+//  Copyright © 2018 geeoku. All rights reserved.
+//
+
+import UIKit
+
+class LeagueTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+  
+  func registerNibFiles() {
+    
+  }
+  
+  
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+
+}
---
+++ FootballNow!/View/LeagueTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LeagueTableViewCell.swift
+//  FootballNow!
+//
+//  Created by Fredrick Ohen on 1/4/18.
+//  Copyright © 2018 geeoku. All rights reserved.
+//
+
+import UIKit
+
+class LeagueTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+}
