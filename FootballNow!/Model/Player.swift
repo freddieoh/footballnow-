3ac62db13f5a494700abaaafabaad2b59fcb339e
Fredrick Ohen <fredrickohen@gmail.com>
2018-01-04 11:49:36 -0600

    Added Player Model

---
+++ FootballNow!/Model/Player.swift
@@ -0,0 +1,21 @@
+//
+//  Player.swift
+//  FootballNow!
+//
+//  Created by Fredrick Ohen on 1/4/18.
+//  Copyright © 2018 geeoku. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+  
+  let name: String
+  let position: String
+  let jerseyNumber: Int
+  let dateOfBirth: String
+  let nationality: String
+  
+  
+}
