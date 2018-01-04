8064e414d9849aa9fd194268c2306ba4c9a825e6
Fredrick Ohen <fredrickohen@gmail.com>
2018-01-04 13:09:18 -0600

    Refactored PlayerTableViewCell xib

---
+++ FootballNow!/View/PlayerTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PlayerTableViewCell.swift
+//  FootballNow!
+//
+//  Created by Fredrick Ohen on 1/4/18.
+//  Copyright © 2018 geeoku. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var positionLabel: UILabel!
+  @IBOutlet weak var dateOfBirthLabel: UILabel!
+  @IBOutlet weak var jerseyNumberLabel: UILabel!
+  @IBOutlet weak var nationalityLabel: UILabel!
+  @IBOutlet weak var contractUntilLabel: UILabel!
+  
+  
+  override func awakeFromNib() {
+        super.awakeFromNib()
+
+  }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
