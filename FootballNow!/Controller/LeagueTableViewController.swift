27cd06a3f0ebe75b0f91b77a6de60438a91ee82c
Fredrick Ohen <fredrickohen@gmail.com>
2018-01-08 10:08:52 -0600

    Loaded LeagueTableViewCell nib in LeagueTableView

--- FootballNow!/Controller/LeagueTableViewController.swift
+++ FootballNow!/Controller/LeagueTableViewController.swift
@@ -12,25 +12,33 @@ class LeagueTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      registerNibFiles()
 
     }
   
   func registerNibFiles() {
-    
+    tableView.register(UINib(nibName: "LeagueTableViewCell", bundle: nil), forCellReuseIdentifier: "LeagueCell")
   }
-  
-  
 
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
-
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "LeagueCell", for: indexPath) as! LeagueTableViewCell
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 327
+  }
 
 }
